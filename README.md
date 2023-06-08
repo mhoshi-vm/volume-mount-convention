b4d16b934bd3668be37643a6c4eaca389e787cf6
machih <machih@vmware.com>
2023-06-09 00:55:07 +0900

    Update podintent.yaml

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ```
-tanzu apps workload apply --label apps.tanzu.vmware.com/has-tests=true --git-repo https://github.com/mhoshi-vm/volume-mount-convention --git-branch main --type worker volumemountconvention -n convention-demo
+tanzu apps workload apply --build-env "BP_JVM_VERSION=17"  --annotation autoscaling.knative.dev/minScale=1  --label apps.tanzu.vmware.com/has-tests=true --git-repo https://github.com/mhoshi-vm/volume-mount-convention --git-branch main --type web volumemountconvention -n convention-demo
 ```
--- src/main/java/jp/vmware/tanzu/pythonfuncconvention/convention/VolumeMountConvention.java
+++ src/main/java/jp/vmware/tanzu/pythonfuncconvention/convention/VolumeMountConvention.java
@@ -14,54 +14,56 @@ import java.util.Map;
 @Primary
 public class VolumeMountConvention implements Convention {
 
-	@Override
-	public PodConventionContextStatus handler(PodConventionContext podConventionContext) {
-		String[] appliedConventions = { "volumeMountConvention" };
+    @Override
+    public PodConventionContextStatus handler(PodConventionContext podConventionContext) {
+        String[] appliedConventions = {"volumeMountConvention"};
 
-		V1PodTemplateSpec podTemplateSpec = podConventionContext.spec().template();
+        V1PodTemplateSpec podTemplateSpec = podConventionContext.spec().template();
 
-		V1ObjectMeta meta = podConventionContext.metadata();
-		Map<String, String> labels = meta.getLabels();
+        V1ObjectMeta meta = podTemplateSpec.getMetadata();
+        if (meta != null) {
+            Map<String, String> labels = meta.getLabels();
 
-		if (labels != null &&
-				labels.get("jp.vmware.tanzu/volumemount") != null &&
-				labels.get("jp.vmware.tanzu/volumemount").equals("nfs") &&
-				labels.get("jp.vmware.tanzu/volumemount.server") != null &&
-				labels.get("jp.vmware.tanzu/volumemount.path") !=null &&
-				labels.get("jp.vmware.tanzu/volumemount.mountpath") !=null)	{
+            if (labels != null &&
+                    labels.get("jp.vmware.tanzu/volumemount") != null &&
+                    labels.get("jp.vmware.tanzu/volumemount").equals("nfs") &&
+                    labels.get("jp.vmware.tanzu/volumemount.server") != null &&
+                    labels.get("jp.vmware.tanzu/volumemount.path") != null &&
+                    labels.get("jp.vmware.tanzu/volumemount.mountpath") != null) {
 
-			V1Volume volume = new V1Volume();
-			V1NFSVolumeSource nfsVolumeSource = new V1NFSVolumeSource();
-			nfsVolumeSource.setServer(labels.get("jp.vmware.tanzu/volumemount.server"));
-			nfsVolumeSource.setPath(labels.get("jp.vmware.tanzu/volumemount.path"));
+                V1Volume volume = new V1Volume();
+                V1NFSVolumeSource nfsVolumeSource = new V1NFSVolumeSource();
+                nfsVolumeSource.setServer(labels.get("jp.vmware.tanzu/volumemount.server"));
+                nfsVolumeSource.setPath(labels.get("jp.vmware.tanzu/volumemount.path"));
 
-			volume.setName("nfs-mount");
-			volume.setNfs(nfsVolumeSource);
+                volume.setName("nfs-mount");
+                volume.setNfs(nfsVolumeSource);
 
-			V1PodSpec podSpec = podTemplateSpec.getSpec();
-			if (podSpec != null) {
-				List<V1Volume> volumeList = podSpec.getVolumes() != null ?
-						podSpec.getVolumes() :
-						new ArrayList<>();
-				volumeList.add(volume);
-				podSpec.setVolumes(volumeList);
+                V1PodSpec podSpec = podTemplateSpec.getSpec();
+                if (podSpec != null) {
+                    List<V1Volume> volumeList = podSpec.getVolumes() != null ?
+                            podSpec.getVolumes() :
+                            new ArrayList<>();
+                    volumeList.add(volume);
+                    podSpec.setVolumes(volumeList);
 
-				for (V1Container container : podSpec.getContainers()) {
-					List<V1VolumeMount> volumeMounts =
-							container.getVolumeMounts() != null ?
-									container.getVolumeMounts() :
-									new ArrayList<>();
-					V1VolumeMount volumeMount = new V1VolumeMount();
-					volumeMount.setName("nfs-mount");
-					volumeMount.setMountPath(labels.get("jp.vmware.tanzu/volumemount.mountpath"));
-					volumeMounts.add(volumeMount);
-					container.setVolumeMounts(volumeMounts);
-				}
-			}
+                    for (V1Container container : podSpec.getContainers()) {
+                        List<V1VolumeMount> volumeMounts =
+                                container.getVolumeMounts() != null ?
+                                        container.getVolumeMounts() :
+                                        new ArrayList<>();
+                        V1VolumeMount volumeMount = new V1VolumeMount();
+                        volumeMount.setName("nfs-mount");
+                        volumeMount.setMountPath(labels.get("jp.vmware.tanzu/volumemount.mountpath"));
+                        volumeMounts.add(volumeMount);
+                        container.setVolumeMounts(volumeMounts);
+                    }
+                }
+            }
 
-		}
+        }
 
-		return new PodConventionContextStatus(podTemplateSpec, appliedConventions);
-	}
+        return new PodConventionContextStatus(podTemplateSpec, appliedConventions);
+    }
 
 }
